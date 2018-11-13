41f1d02722351f0a37b77a74e89a100939e4bfa6
mr0zek <szymon.szylhabel@gmail.com>
2018-11-13 12:01:40 +0100

    ??

--- DDD.Base/Domain/AggregateRoot.cs
+++ DDD.Base/Domain/AggregateRoot.cs
@@ -4,6 +4,24 @@ namespace DDD.Base.Domain
 {
   public abstract class AggregateRoot
   {
+    protected bool Equals(AggregateRoot other)
+    {
+      return Equals(AggregateId, other.AggregateId);
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((AggregateRoot) obj);
+    }
+
+    public override int GetHashCode()
+    {
+      return (AggregateId != null ? AggregateId.GetHashCode() : 0);
+    }
+
     public enum AggregateStatus
     {
       ACTIVE, ARCHIVE
